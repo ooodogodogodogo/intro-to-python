a7eb28ab9881bcaa1665269806619b199a29a1f9
ooodogodogodogo <dogukanturksoy1999@gmail.com>
2023-09-27 11:29:36 +0300

    first commit

---
+++ course1/lecture1.py
@@ -0,0 +1,49 @@
+"""""
+fonksiyonlar nasıl işliyor?
+1: fonksiyonu tanımla, başında def var. Sonrasında fonksiyonun adı var. Sonrasında parantez içinde parametreler var.
+2: İçindeki satırları işletiyor.
+3: Bir veya birden fazla değer dönüyor.
+
+def hello_world(name1,name2= "Dogukan"):
+    return "Hello World, we are " + name1 + " and " + name2 + " and we are learning Python"
+
+message = hello_world("Busra","Dogo")
+print(message)
+
+def hello_world(name1,name2= "Dogukan"):
+    return "tekrar yazdim zaaa"
+
+message = hello_world("Busra","Dogo")
+print(message)
+"""""
+
+#print("starting")
+#print("completed")
+
+#a= "hello world" // string
+#b= a.capitalize()
+#print(b)
+  
+#x =5 /int
+#y =3
+#print(type(x/y)) /float (kesirli sayi)
+#print((x/y)) # kendime not 
+#word1 = "World"
+#print(int(z)+3)
+#print(x**y)
+#word2  = "world"
+#print(type('a'))
+#print(word1 == word2)
+
+# a= True /boolean (dogru yanlis)
+# type(a) a nın veri tipini verir
+# str() int() float() bool() veri tipini degistirir
+# print bastırıyor.
+# if elselere baktık.
+def calculate_circle_area(radius):
+    #burada fonksiyon olacak
+    return #burada sonuç dönecek
+r = 5
+calculate_circle_area(r)
+
+    
