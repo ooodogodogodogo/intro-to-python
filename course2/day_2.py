680eff90d129bd0bb51833097567f42472aa6a02
busrasturker <bs.turker@hotmail.com>
2023-09-29 09:39:04 +0300

    first codr

---
+++ course2/day_2.py
@@ -0,0 +1,48 @@
+first_name = 'busra'
+last_name = 'turker'
+full_name = 'busra turker'
+country = 'turkey'
+city = 'istanbul'
+age = 32
+year = 2023
+is_maried = False
+person_info = { 
+               'first_name': 'busra',
+               'last_name' : 'turker',
+                'full_name' : 'busra turker',
+                'country' : 'turkey',
+                'city' : 'istanbul',
+                'age': 32,
+                'year': 2023,
+                'is_maried' : 'False'
+               }
+tcno = {"10201523048" : person_info}
+print ('first_name length:', len(first_name))
+print ('last_name ', last_name)
+print('last_name length:', len(last_name))
+print('full_name', full_name)
+print('full_name length:', len(full_name))
+print('country', country)
+print('country legenth:', len(country ))
+print( first_name, last_name,full_name , country )
+
+
+a = 5
+b = 4
+mulp = a + b
+print ('mulp=', mulp)
+c = a*b
+print('c',c)
+d=a//b
+print('d',d)
+e=a**b
+print('e',e)
+f=a%b
+print('f',f)
+
+r=30
+pi= 3
+A=pi*(r**2)
+print('A',A)
+c=2*pi*r
+print('c',c)
